9e9cb11e6649be970ae9fc02aeb78677e05fcab3
itzzmohib31 <mohibplayz31@gmail.com>
2023-05-31 21:08:02 +0500

    theme fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,45 +12,40 @@ import ContactForm from './components/ContactForm';
 import Offerings from './components/Offerings/Offerings';
 
 export default function Home() {
+  const currentTheme = localStorage.getItem("Theme") ?? "light";
 
-  
-  const currentTheme=localStorage.getItem("Theme");
+  const [theme, setTheme] = useState(currentTheme);
 
-  const [theme,setTheme]=useState(currentTheme);
-
-  useEffect(()=>{
-    if(window.matchMedia('(prefers-color-scheme:dark)').matches){
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
       setTheme("dark");
-    }
-    else{
+    } else {
       setTheme("light");
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    if(currentTheme==="dark"){
-      document.documentElement.classList.add("dark")
+  useEffect(() => {
+    if (currentTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
-    else
-    {
-      document.documentElement.classList.remove("dark")
-    }
-  },[currentTheme])
+  }, [currentTheme]);
 
-  const themeSwitch=()=>{
-    setTheme(theme==="dark"?"light":"dark");
-    localStorage.setItem('Theme',theme);
-  }
+  const themeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+    localStorage.setItem("Theme", theme);
+  };
 
   return (
-    <main className='dark:bg-black dark:text-white '>
-    <Navigation theme={themeSwitch}></Navigation>
-    <HeroSection></HeroSection>
-    <Offerings></Offerings>
-    <Timeline/>
-    <Visions></Visions>
-    <Services></Services>
-    <ContactForm></ContactForm>
+    <main className="dark:bg-black dark:text-white ">
+      <Navigation theme={themeSwitch}></Navigation>
+      <HeroSection></HeroSection>
+      <Offerings></Offerings>
+      <Timeline />
+      <Visions></Visions>
+      <Services></Services>
+      <ContactForm></ContactForm>
     </main>
-  )
+  );
 }
