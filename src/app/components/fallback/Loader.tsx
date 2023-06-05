be4d30511bee9df5e220317f8901e5002817ac69
itzzmohib31 <mohibplayz31@gmail.com>
2023-06-05 19:32:58 +0500

    changes

--- src/app/components/fallback/Loader.tsx
+++ src/app/components/fallback/Loader.tsx
@@ -3,12 +3,10 @@ import { InfinitySpin } from "react-loader-spinner";
 
 const Loader=()=>{
 return(
-<div style={{width:'20%', display:'flex',margin:'auto',alignItems:'center',height:'200px'}}>
 <InfinitySpin 
   width='100%'
   color="#005cff"
 />
-</div>
 
 )
 }
